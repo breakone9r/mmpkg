6e35ae052685633f3963f605275213ebc45d1b62
Chuck Burns <break19@gmail.com>
2020-08-23 10:35:48 -0500

    removed spurious semi-colon

--- mmpkg/src/triggers/mmpkg/fake-sextant.lua
+++ mmpkg/src/triggers/mmpkg/fake-sextant.lua
@@ -2,7 +2,7 @@ if (gmcp.room.info.zone == "Alyria") or (gmcp.room.info.zone == "Faerie Plane Wi
     local length = matches[2]:len()
     local coords = gmcp.room.info.coord.x .. ", " .. gmcp.room.info.coord.y
     local dashlength = 44 - string.len(coords)
-    local coordinsert = "<blue>;-[ <gray>" .. coords .. "<blue> ]" .. string.rep("-",dashlength)
+    local coordinsert = "<blue>-[ <gray>" .. coords .. "<blue> ]" .. string.rep("-",dashlength)
     selectCaptureGroup(2)
     creplace(coordinsert)
     deselect()
