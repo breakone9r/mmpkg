9fc0da19563fdcb5f64e219cd3b290fa30daf157
Chuck Burns <break19@gmail.com>
2020-08-22 20:39:07 -0500

    Update 0.7.9 - cleaned up some poor formatting

--- mmpkg/src/scripts/mmpkg/mmpkg.updates.lua
+++ mmpkg/src/scripts/mmpkg/mmpkg.updates.lua
@@ -6,18 +6,18 @@ function mmpkg.checkforUpdate()
     noop()
   end
 
-  function mmpkg.updateMap()
-    cecho("\n<white>Downloading the latest map: https://github.com/breakone9r/mmpkg/raw/master/mm_map.dat\n<green>This may take a bit. It's a large map.")
-    local saveto = getMudletHomeDir() .. "/map/mm_map.dat"
-    local url = "https://github.com/breakone9r/mmpkg/raw/master/mm_map.dat"    
-    registerAnonymousEventHandler("sysDownloadDone", "mmpkg.mapdownloaded")
+function mmpkg.mapdownloaded(_,filename)
+  if not filename:find("mm_map.dat", 1, true) then
+    return
   end
-
-  function mmpkg.mapdownloaded(_,filename)
-    if not filename:find("mm_map.dat", 1, true) then
-      return
-    end
-    cecho("<green>DONE! Loading your new map.")
-    loadMap(getMudletHomeDir() .. "/map/mm_map.dat")
-  end
+  cecho("<green>DONE! Loading your new map.")
+  loadMap(getMudletHomeDir() .. "/map/mm_map.dat")
+end
   
+function mmpkg.updateMap()
+  cecho("\n<white>Downloading the latest map: <gray>https://github.com/breakone9r/mmpkg/raw/master/mm_map.dat\n<green>This may take a bit. It's a large map.")
+  local saveto = getMudletHomeDir() .. "/map/mm_map.dat"
+  local url = "https://github.com/breakone9r/mmpkg/raw/master/mm_map.dat"    
+  registerAnonymousEventHandler("sysDownloadDone", "mmpkg.mapdownloaded")
+  downloadFile(saveto, url)
+end  
