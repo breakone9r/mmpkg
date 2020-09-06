28e2c22e35d7b0b812b80d8853616d8fc3acd77c
Chuck Burns <break19@gmail.com>
2020-09-05 19:18:59 -0500

    Added auto-linkification of websites in chat

---
+++ mmpkg/src/triggers/mmpkg/Linkify.lua
@@ -0,0 +1,10 @@
+local site = matches[1]
+if string.ends(site, "'") then site = site:sub(1, -2) end
+selectString(site, 1)
+if not string.starts(string.lower(site), "http") then
+    site = "http://" .. string.lower(site)
+end
+local command = string.format([[openWebPage("%s")]], site)
+setUnderline(true)
+setLink(command, "Open link in browser.")
+resetFormat()
