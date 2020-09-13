d502d52ff4adc0e35967742a173f70e53a59a8ad
Chuck Burns <break19@notaryzen.localdomain>
2020-09-12 21:49:32 -0500

    Add Scan Helper + more options to config

--- mmpkg/src/aliases/mmpkg/mmpkg.lua
+++ mmpkg/src/aliases/mmpkg/mmpkg.lua
@@ -1,53 +1,397 @@
 if (matches[2] == "config") then
-  if (matches[3] == "fontsize") then
-    cecho("\n<white>Setting fontsize to: " .. matches[4] .. "\n")
-    mmpkg.conf.fontsize = tonumber(matches[4])
-    setFontSize(tonumber(matches[4]))
-    mmpkg.Captures:setFontSize(tonumber(matches[4]))
-    table.save(getMudletHomeDir() .. "/mmpkg.conf", mmpkg.conf)
-  elseif (matches[3] == "timestamps") then
-    if (mmpkg.conf.timestamps == true) then
-      cecho("\n<white>Disabling Timestamps\n")
-      mmpkg.conf.timestamps = false
-      mmpkg.Captures:disableTimestamp()
-      table.save(getMudletHomeDir() .. "/mmpkg.conf", mmpkg.conf)
-    else
-      cecho("\n<white>Enabling Timestamps\n")
-      mmpkg.conf.timestamps = true
-      mmpkg.Captures:enableTimestamp()
-      table.save(getMudletHomeDir() .. "/mmpkg.conf", mmpkg.conf)
-    end
-  elseif (matches[3] == "tabs") then
-    cecho("\n<white:red>Sorry, Disabling/Enabling Tabs is not yet supported.\n")
-  elseif (matches[3] == "localonly") then
-    if (mmpkg.conf.areaonly == false) then
-      cecho("\n<yellow>mapper where <white> now searches only local rooms\n")
-      mmpkg.conf.areaonly = true
-      table.save(getMudletHomeDir() .. "/mmpkg.conf", mmpkg.conf)
-    else
-      cecho("\n<yellow>mapper where <white> now searches all rooms\n")
-      mmpkg.conf.areaonly = false
-      table.save(getMudletHomeDir() .. "/mmpkg.conf", mmpkg.conf)
-    end
+  mmpkg.config = mmpkg.config or {}
+  mmpkg.confwin = 
+  Geyser.UserWindow:new(
+    {
+      name = "mmpkg.confwin",
+      titleText = "mmpkg Configuration",
+      x = "10%",
+      y = "20%",
+      width = 600,
+      height = 400,
+      color = "gray",
+    }
+  )
+  mmpkg.confwin:disableAutoDock()
+  mmpkg.config.genoptions = 
+  Geyser.Label:new(
+    {
+      name = "mmpkg.config.genoptions",
+      x = 29,
+      y = 21,
+      fgColor = "black",
+      color = "gray",
+      width = 250,
+      height = 20,
+      message = [[General Options]],
+    },
+    mmpkg.confwin
+  )
+  mmpkg.config.timestampbox = 
+  Geyser.Label:new(
+    {name = "mmpkg.config.timestampbox", x = 10, y = 50, width = 21, height = 19}, mmpkg.confwin
+  )
+  mmpkg.config.timestamplabel = 
+  Geyser.Label:new(
+    {
+      name = "mmpkg.config.timestamplabel",
+      x = 32,
+      y = 52,
+      fgColor = "black",
+      color = "gray",
+      width = 200,
+      height = 20,
+      message = [[Timestamps on in Captures window?]],
+    },
+    mmpkg.confwin
+  )
+  mmpkg.config.timestampbox:setStyleSheet([[
+    background-color: rgba(0,0,0,0%);
+  ]])
+  if mmpkg.conf.timestamps then
+    mmpkg.config.timestampbox:setBackgroundImage(mmpkg.resources .. [[/checkbox_ticked.png]])
   else
-    local TimeStamps = {}
-    local LocalOnly = {}
-    if mmpkg.conf.timestamps == true then
-      TimeStamps = "On"
-    else
-      TimeStamps = "Off"
-    end
-    if mmpkg.conf.areaonly == true then
-      LocalOnly = "mapper where shows only local results"
-    else
-      LocalOnly = "mapper where shows all results"
-    end
-    cecho("\n<cyan>mmpkg config settings:")
-    cecho("\n<green>    LocalOnly  : " .. LocalOnly)
-    cecho("\n<green>    Fontsize   : " .. mmpkg.conf.fontsize)
-    cecho("\n<green>    Timestamps : " .. TimeStamps)
-    cecho("\n<green>    Tabs       : " .. table.concat(mmpkg.conf.tabs, " ") .. "\n")
+    mmpkg.config.timestampbox:setBackgroundImage(mmpkg.resources .. [[/checkbox_unticked.png]])
   end
+  mmpkg.config.timestampbox:setClickCallback("mmpkg.config.timestampbox_click")
+  mmpkg.config.areaonlybox = 
+  Geyser.Label:new(
+    {name = "mmpkg.config.areaonlybox", x = 10, y = 70, width = 21, height = 19}, mmpkg.confwin
+  )
+  mmpkg.config.areaonlylabel = 
+  Geyser.Label:new(
+    {
+      name = "mmpkg.config.areaonlylabel",
+      x = 32,
+      y = 72,
+      fgColor = "black",
+      color = "gray",
+      width = 220,
+      height = 20,
+      message = [[Show only local rooms in 'mapper where'?]],
+    },
+    mmpkg.confwin
+  )
+  mmpkg.config.areaonlybox:setStyleSheet([[
+    background-color: rgba(0,0,0,0%);
+  ]])
+  if mmpkg.conf.areaonly then
+    mmpkg.config.areaonlybox:setBackgroundImage(mmpkg.resources .. [[/checkbox_ticked.png]])
+  else
+    mmpkg.config.areaonlybox:setBackgroundImage(mmpkg.resources .. [[/checkbox_unticked.png]])
+  end
+  mmpkg.config.areaonlybox:setClickCallback("mmpkg.config.areaonlybox_click")
+
+  mmpkg.config.scanhelperbox = 
+  Geyser.Label:new(
+    {name = "mmpkg.config.scanhelperbox", x = 10, y = 90, width = 21, height = 19}, mmpkg.confwin
+  )
+  mmpkg.config.scanhelperlabel = 
+  Geyser.Label:new(
+    {
+      name = "mmpkg.config.scanhelperlabel",
+      x = 32,
+      y = 92,
+      fgColor = "black",
+      color = "gray",
+      width = 220,
+      height = 20,
+      message = [[Enable Scan Helper?]],
+    },
+    mmpkg.confwin
+  )
+  mmpkg.config.scanhelperbox:setStyleSheet([[
+    background-color: rgba(0,0,0,0%);
+  ]])
+  if isActive("Scan Helper", "trigger") then
+    mmpkg.config.scanhelperbox:setBackgroundImage(mmpkg.resources .. [[/checkbox_ticked.png]])
+  else
+    mmpkg.config.scanhelperbox:setBackgroundImage(mmpkg.resources .. [[/checkbox_unticked.png]])
+  end
+  mmpkg.config.scanhelperbox:setClickCallback("mmpkg.config.scanhelperbox_click")
+
+  mmpkg.config.linkifybox = 
+  Geyser.Label:new(
+    {name = "mmpkg.config.linkifybox", x = 10, y = 110, width = 21, height = 19}, mmpkg.confwin
+  )
+  mmpkg.config.linkifylabel = 
+  Geyser.Label:new(
+    {
+      name = "mmpkg.config.linkifylabel",
+      x = 32,
+      y = 112,
+      fgColor = "black",
+      color = "gray",
+      width = 220,
+      height = 20,
+      message = [[Web addresses in chat are clickable?]],
+    },
+    mmpkg.confwin
+  )
+  mmpkg.config.linkifybox:setStyleSheet([[
+    background-color: rgba(0,0,0,0%);
+  ]])
+  if isActive("Linkify") then
+    mmpkg.config.linkifybox:setBackgroundImage(mmpkg.resources .. [[/checkbox_ticked.png]])
+  else
+    mmpkg.config.linkifybox:setBackgroundImage(mmpkg.resources .. [[/checkbox_unticked.png]])
+  end
+  mmpkg.config.linkifybox:setClickCallback("mmpkg.config.linkifybox_click")
+
+  mmpkg.config.maintabs = 
+  Geyser.Label:new(
+    {
+      name = "mmpkg.config.maintabs",
+      x = 350,
+      y = 21,
+      width = 250,
+      height = 20,
+      fgColor = "black",
+      color = "gray",
+      message = [[Select which tabs to display in Captures]],
+    },
+    mmpkg.confwin
+  )
+  mmpkg.config.Alliancebox = 
+  Geyser.Label:new(
+    {name = "mmpkg.config.Alliancebox", x = 340, y = 50, width = 21, height = 19}, mmpkg.confwin
+  )
+  mmpkg.config.Alliancelabel = 
+  Geyser.Label:new(
+    {
+      name = "mmpkg.config.Alliancelabel",
+      x = 370,
+      y = 52,
+      fgColor = "black",
+      color = "gray",
+      width = 220,
+      height = 20,
+      message = [[Alliance]],
+    },
+    mmpkg.confwin
+  )
+  mmpkg.config.Alliancebox:setStyleSheet([[
+    background-color: rgba(0,0,0,0%);
+  ]])
+  if table.contains(mmpkg.conf.tabs, "Alliance") then
+    mmpkg.config.alliance = true
+    mmpkg.config.Alliancebox:setBackgroundImage(mmpkg.resources .. [[/checkbox_ticked.png]])
+  else
+    mmpkg.config.alliance = false
+    mmpkg.config.Alliancebox:setBackgroundImage(mmpkg.resources .. [[/checkbox_unticked.png]])
+  end
+  mmpkg.config.Alliancebox:setClickCallback("mmpkg.config.Alliancebox_click")
+  mmpkg.config.Clanbox = 
+  Geyser.Label:new(
+    {name = "mmpkg.config.Clanbox", x = 340, y = 70, width = 21, height = 19}, mmpkg.confwin
+  )
+  mmpkg.config.Clanlabel = 
+  Geyser.Label:new(
+    {
+      name = "mmpkg.config.Clanlabel",
+      x = 370,
+      y = 72,
+      fgColor = "black",
+      color = "gray",
+      width = 220,
+      height = 20,
+      message = [[Clan]],
+    },
+    mmpkg.confwin
+  )
+  mmpkg.config.Clanbox:setStyleSheet([[
+    background-color: rgba(0,0,0,0%);
+  ]])
+  if table.contains(mmpkg.conf.tabs, "Clan") then
+    mmpkg.config.clan = true
+    mmpkg.config.Clanbox:setBackgroundImage(mmpkg.resources .. [[/checkbox_ticked.png]])
+  else
+    mmpkg.config.clan = false
+    mmpkg.config.Clanbox:setBackgroundImage(mmpkg.resources .. [[/checkbox_unticked.png]])
+  end
+  mmpkg.config.Clanbox:setClickCallback("mmpkg.config.Clanbox_click")
+  mmpkg.config.Novicebox = 
+  Geyser.Label:new(
+    {name = "mmpkg.config.Novicebox", x = 340, y = 90, width = 21, height = 19}, mmpkg.confwin
+  )
+  mmpkg.config.Novicelabel = 
+  Geyser.Label:new(
+    {
+      name = "mmpkg.config.Novicelabel",
+      x = 370,
+      y = 92,
+      fgColor = "black",
+      color = "gray",
+      width = 220,
+      height = 20,
+      message = [[Novice]],
+    },
+    mmpkg.confwin
+  )
+  mmpkg.config.Novicebox:setStyleSheet([[
+    background-color: rgba(0,0,0,0%);
+  ]])
+  if table.contains(mmpkg.conf.tabs, "Novice") then
+    mmpkg.config.novice = true
+    mmpkg.config.Novicebox:setBackgroundImage(mmpkg.resources .. [[/checkbox_ticked.png]])
+  else
+    mmpkg.config.novice = false
+    mmpkg.config.Novicebox:setBackgroundImage(mmpkg.resources .. [[/checkbox_unticked.png]])
+  end
+  mmpkg.config.Novicebox:setClickCallback("mmpkg.config.Novicebox_click")
+
+  mmpkg.config.Tellsbox = 
+  Geyser.Label:new(
+    {name = "mmpkg.config.Tellsbox", x = 340, y = 110, width = 21, height = 19}, mmpkg.confwin
+  )
+  mmpkg.config.Tellslabel = 
+  Geyser.Label:new(
+    {
+      name = "mmpkg.config.TellsLabel",
+      x = 370,
+      y = 112,
+      fgColor = "black",
+      color = "gray",
+      width = 220,
+      height = 20,
+      message = [[Tells]],
+    },
+    mmpkg.confwin
+  )
+  mmpkg.config.Tellsbox:setStyleSheet([[
+    background-color: rgba(0,0,0,0%);
+  ]])
+  if table.contains(mmpkg.conf.tabs, "Tells") then
+    mmpkg.config.tells = true
+    mmpkg.config.Tellsbox:setBackgroundImage(mmpkg.resources .. [[/checkbox_ticked.png]])
+  else
+    mmpkg.config.tells = false
+    mmpkg.config.Tellsbox:setBackgroundImage(mmpkg.resources .. [[/checkbox_unticked.png]])
+  end
+  mmpkg.config.Tellsbox:setClickCallback("mmpkg.config.Tellsbox_click")
+
+  mmpkg.config.Formbox = 
+  Geyser.Label:new(
+    {name = "mmpkg.config.Formbox", x = 340, y = 130, width = 21, height = 19}, mmpkg.confwin
+  )
+  mmpkg.config.Formlabel = 
+  Geyser.Label:new(
+    {
+      name = "mmpkg.config.Formlabel",
+      x = 370,
+      y = 132,
+      fgColor = "black",
+      color = "gray",
+      width = 220,
+      height = 20,
+      message = [[Form]],
+    },
+    mmpkg.confwin
+  )
+  mmpkg.config.Formbox:setStyleSheet([[
+    background-color: rgba(0,0,0,0%);
+  ]])
+  if table.contains(mmpkg.conf.tabs, "Form") then
+    mmpkg.config.form = true
+    mmpkg.config.Formbox:setBackgroundImage(mmpkg.resources .. [[/checkbox_ticked.png]])
+  else
+    mmpkg.config.form = false
+    mmpkg.config.Formbox:setBackgroundImage(mmpkg.resources .. [[/checkbox_unticked.png]])
+  end
+  mmpkg.config.Formbox:setClickCallback("mmpkg.config.Formbox_click")
+
+  mmpkg.config.Relaysbox = 
+  Geyser.Label:new(
+    {name = "mmpkg.config.Relaysbox", x = 340, y = 150, width = 21, height = 19}, mmpkg.confwin
+  )
+  mmpkg.config.Relayslabel = 
+  Geyser.Label:new(
+    {
+      name = "mmpkg.config.Relayslabel",
+      x = 370,
+      y = 152,
+      fgColor = "black",
+      color = "gray",
+      width = 220,
+      height = 20,
+      message = [[Relays]],
+    },
+    mmpkg.confwin
+  )
+  mmpkg.config.Relaysbox:setStyleSheet([[
+    background-color: rgba(0,0,0,0%);
+  ]])
+  if table.contains(mmpkg.conf.tabs, "Relays") then
+    mmpkg.config.relays = true
+    mmpkg.config.Relaysbox:setBackgroundImage(mmpkg.resources .. [[/checkbox_ticked.png]])
+  else
+    mmpkg.config.relays = false
+    mmpkg.config.Relaysbox:setBackgroundImage(mmpkg.resources .. [[/checkbox_unticked.png]])
+  end
+  mmpkg.config.Relaysbox:setClickCallback("mmpkg.config.Relaysbox_click")
+
+  mmpkg.config.Talkbox = 
+  Geyser.Label:new(
+    {name = "mmpkg.config.Talkbox", x = 340, y = 170, width = 21, height = 19}, mmpkg.confwin
+  )
+  mmpkg.config.Talklabel = 
+  Geyser.Label:new(
+    {
+      name = "mmpkg.config.Talklabel",
+      x = 370,
+      y = 172,
+      fgColor = "black",
+      color = "gray",
+      width = 220,
+      height = 20,
+      message = [[Talk]],
+    },
+    mmpkg.confwin
+  )
+  mmpkg.config.Talkbox:setStyleSheet([[
+    background-color: rgba(0,0,0,0%);
+  ]])
+  if table.contains(mmpkg.conf.tabs, "Talk") then
+    mmpkg.config.talk = true
+    mmpkg.config.Talkbox:setBackgroundImage(mmpkg.resources .. [[/checkbox_ticked.png]])
+  else
+    mmpkg.config.talk = false
+    mmpkg.config.Talkbox:setBackgroundImage(mmpkg.resources .. [[/checkbox_unticked.png]])
+  end
+  mmpkg.config.Talkbox:setClickCallback("mmpkg.config.Talkbox_click")
+
+  mmpkg.config.savebutton = 
+  Geyser.Label:new(
+    {
+      name = "mmpkg.config.savebutton",
+      x = 30,
+      y = -50,
+      width = 80,
+      height = 40,
+      fgColor = "black",
+      color = "white",
+      message = [[<center>Save&nbsp;</center>]],
+    },
+    mmpkg.confwin
+  )
+  mmpkg.config.savebutton:setStyleSheet(GUI.BoxCSS:getCSS())
+  mmpkg.config.savebutton:setClickCallback("mmpkg.config.savebtn_click")
+  mmpkg.config.cancelbutton = 
+  Geyser.Label:new(
+    {
+      name = "mmpkg.config.cancelbutton",
+      x = 90,
+      y = -50,
+      width = 80,
+      height = 40,
+      fgColor = "black",
+      color = "red",
+      message = [[<center>Cancel&nbsp;</center>]],
+    },
+    mmpkg.confwin
+  )
+  mmpkg.config.cancelbutton:setStyleSheet(GUI.BoxCSS:getCSS())
+  mmpkg.config.cancelbutton:setClickCallback("mmpkg.config.cancelbtn_click")
 elseif (matches[2] == "pages") then
   if not mmpkg.PagesCon then
     mmpkg.PagesCon = 
---
+++ mmpkg/src/scripts/mmpkg/mmpkg.configwin.lua
@@ -0,0 +1,181 @@
+mmpkg = mmpkg or {}
+mmpkg.config = mmpkg.config or {}
+
+function mmpkg.config.timestampbox_click()
+  if mmpkg.conf.timestamps then
+    mmpkg.conf.timestamps = false
+    mmpkg.config.timestampbox:setBackgroundImage(mmpkg.resources .. [[/checkbox_unticked.png]])
+  else
+    mmpkg.conf.timestamps = true
+    mmpkg.config.timestampbox:setBackgroundImage(mmpkg.resources .. [[/checkbox_ticked.png]])
+  end
+end
+
+function mmpkg.config.scanhelperbox_click()
+  if isActive("Scan Helper", "trigger") then
+    disableTrigger("Scan Helper")
+  else
+    enableTrigger("Scan Helper")
+  end
+end
+
+function mmpkg.config.linkifybox_click()
+  if isActive("Linkify", "trigger") then
+    disableTrigger("Linkify")
+  else
+    enableTrigger("Linkify")
+  end
+end
+
+function mmpkg.config.areaonlybox_click()
+  if mmpkg.conf.areaonly then
+    mmpkg.conf.areaonly = false
+    mmpkg.config.areaonlybox:setBackgroundImage(mmpkg.resources .. [[/checkbox_unticked.png]])
+  else
+    mmpkg.conf.areaonly = true
+    mmpkg.config.areaonlybox:setBackgroundImage(mmpkg.resources .. [[/checkbox_ticked.png]])
+  end
+end
+
+function mmpkg.config.savebtn_click()
+  table.save(getMudletHomeDir() .. "/mmpkg.conf", mmpkg.conf)
+  mmpkg.confwin:hide()
+  mmpkg.config.applysettings()
+  cecho("\n<green>Saved!")
+end
+
+function mmpkg.config.cancelbtn_click()
+  table.load(getMudletHomeDir() .. "/mmpkg.conf", mmpkg.conf)
+  mmpkg.confwin:hide()
+  cecho("\n<red>No changes saved.")
+end
+
+function mmpkg.config.Alliancebox_click()
+  if mmpkg.config.alliance == true then
+    mmpkg.config.alliance = false
+    mmpkg.config.Alliancebox:setBackgroundImage(mmpkg.resources .. [[/checkbox_unticked.png]])
+  else
+    mmpkg.config.alliance = true
+    mmpkg.config.Alliancebox:setBackgroundImage(mmpkg.resources .. [[/checkbox_ticked.png]])
+  end
+  mmpkg.config.doTabs()
+end
+
+function mmpkg.config.Clanbox_click()
+  if mmpkg.config.clan == true then
+    mmpkg.config.clan = false
+    mmpkg.config.Clan:setBackgroundImage(mmpkg.resources .. [[/checkbox_unticked.png]])
+  else
+    mmpkg.config.clan = true
+    mmpkg.config.Clan:setBackgroundImage(mmpkg.resources .. [[/checkbox_ticked.png]])
+  end
+  mmpkg.config.doTabs()
+end
+
+function mmpkg.config.Novicebox_click()
+  if mmpkg.config.novice == true then
+    mmpkg.config.novice = false
+    mmpkg.config.Novicebox:setBackgroundImage(mmpkg.resources .. [[/checkbox_unticked.png]])
+  else
+    mmpkg.config.novice = true
+    mmpkg.config.Novicebox:setBackgroundImage(mmpkg.resources .. [[/checkbox_ticked.png]])
+  end
+  mmpkg.config.doTabs()
+end
+
+function mmpkg.config.Tellsbox_click()
+  if mmpkg.config.tells == true then
+    mmpkg.config.tells = false
+    mmpkg.config.Tellsbox:setBackgroundImage(mmpkg.resources .. [[/checkbox_unticked.png]])
+  else
+    mmpkg.config.tells = true
+    mmpkg.config.Tellsbox:setBackgroundImage(mmpkg.resources .. [[/checkbox_ticked.png]])
+  end
+  mmpkg.config.doTabs()
+end
+
+function mmpkg.config.Formbox_click()
+  if mmpkg.config.form == true then
+    mmpkg.config.form = false
+    mmpkg.config.Formbox:setBackgroundImage(mmpkg.resources .. [[/checkbox_unticked.png]])
+  else
+    mmpkg.config.form = true
+    mmpkg.config.Formbox:setBackgroundImage(mmpkg.resources .. [[/checkbox_ticked.png]])
+  end
+  mmpkg.config.doTabs()
+end
+
+function mmpkg.config.Relaysbox_click()
+  if mmpkg.config.relays == true then
+    mmpkg.config.relays = false
+    mmpkg.config.Relaysbox:setBackgroundImage(mmpkg.resources .. [[/checkbox_unticked.png]])
+  else
+    mmpkg.config.relays = true
+    mmpkg.config.Relaysbox:setBackgroundImage(mmpkg.resources .. [[/checkbox_ticked.png]])
+  end
+  mmpkg.config.doTabs()
+end
+
+function mmpkg.config.Talkbox_click()
+  if mmpkg.config.talk == true then
+    mmpkg.config.talk = false
+    mmpkg.config.Talkbox:setBackgroundImage(mmpkg.resources .. [[/checkbox_unticked.png]])
+  else
+    mmpkg.config.talk = true
+    mmpkg.config.Talkbox:setBackgroundImage(mmpkg.resources .. [[/checkbox_ticked.png]])
+  end
+  mmpkg.config.doTabs()
+end
+
+function mmpkg.config.doTabs()
+  mmpkg.conf.tabs = {"All"}
+  local tabcount = 1
+  if mmpkg.config.alliance then
+    tabcount = tabcount + 1
+    mmpkg.conf.tabs[tabcount] = "Alliance"
+  end
+  if mmpkg.config.clan then
+    tabcount = tabcount + 1
+    mmpkg.conf.tabs[tabcount] = "Clan"
+  end
+  if mmpkg.config.novice then
+    tabcount = tabcount + 1
+    mmpkg.conf.tabs[tabcount] = "Novice"
+  end
+  if mmpkg.config.tells then
+    tabcount = tabcount + 1
+    mmpkg.conf.tabs[tabcount] = "Tells"
+  end
+  if mmpkg.config.form then
+    tabcount = tabcount + 1
+    mmpkg.conf.tabs[tabcount] = "Form"
+  end
+  if mmpkg.config.relays then
+    tabcount = tabcount + 1
+    mmpkg.conf.tabs[tabcount] = "Relays"
+  end
+  if mmpkg.config.talk then
+    tabcount = tabcount + 1
+    mmpkg.conf.tabs[tabcount] = "Talk"
+  end
+end
+
+function mmpkg.config.applysettings()
+  if (mmpkg.conf.timestamps == true) then
+    mmpkg.Captures:enableTimestamp()
+  else
+    mmpkg.Captures:disableTimestamp()
+  end
+  for _, chan in pairs(mmpkg.Captures.consoles) do
+    if not table.contains(mmpkg.conf.tabs, chan) then
+      mmpkg.Captures:removeTab(chan)
+      disableTrigger(chan)
+    end
+  end
+  for _, chan in spairs(mmpkg.conf.tabs) do
+    if not table.contains(mmpkg.Captures.consoles, chan) then
+      mmpkg.Captures:addTab(chan)
+      enableTrigger(chan)
+    end
+  end
+end
