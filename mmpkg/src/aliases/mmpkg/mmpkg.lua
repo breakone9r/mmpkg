1ccc466b8f43e1363773a39fdaed507619b32b40
Chuck Burns <break19@gmail.com>
2020-09-15 18:15:48 -0500

    fix version mmpkg command

--- mmpkg/src/aliases/mmpkg/mmpkg.lua
+++ mmpkg/src/aliases/mmpkg/mmpkg.lua
@@ -1,45 +1,47 @@
 if (matches[2] == "config") then
   mmpkg.config.doWindow()
-  else if (matches[2] == "ver") then
-    cecho("<green>Current version: ".. mmpkg.packagename)
-  elseif (matches[2] == "pages") then
-    if not mmpkg.PagesCon then
-      mmpkg.PagesCon = 
-      Geyser.UserWindow:new(
-        {
-          name = "mmpkg.PagesCon",
-          titleText = "Pages",
-          x = "20%",
-          y = "20%",
-          width = "50%",
-          height = "40%"
-        }
-      )
+elseif (matches[2] == "ver") then
+  cecho("<green>Current version: ".. mmpkg.packagename)
+elseif (matches[2] == "sounds") then
+  mmpkg.getsounds()
+elseif (matches[2] == "pages") then
+  if not mmpkg.PagesCon then
+    mmpkg.PagesCon = 
+    Geyser.UserWindow:new(
+      {
+        name = "mmpkg.PagesCon",
+        titleText = "Pages",
+        x = "20%",
+        y = "20%",
+        width = "50%",
+        height = "40%"
+      }
+    )
 
-      mmpkg.Pages = 
-      Geyser.MiniConsole:new(
-        {
-          name = "mmpkg.Pages",
-          x = 0,
-          y = 0,
-          autoWrap = true,
-          color = "gray",
-          scrollBar = false,
-          fontSize = 10,
-          width = -5,
-          height = -5
-        },
-        mmpkg.PagesCon
-      )
-    else
-      mmpkg.PagesCon:show()
-    end
-  elseif (matches[2] == "update") then
-    mmpkg.checkupdate(true)
+    mmpkg.Pages = 
+    Geyser.MiniConsole:new(
+      {
+        name = "mmpkg.Pages",
+        x = 0,
+        y = 0,
+        autoWrap = true,
+        color = "gray",
+        scrollBar = false,
+        fontSize = 10,
+        width = -5,
+        height = -5
+      },
+      mmpkg.PagesCon
+    )
   else
-    local cmd = matches[2] or ""
-    cecho("<red:white>ERROR:<white:black> Unknown command '"..cmd.."'!\n")
-    cecho("<yellow>mmpkg pages  :<white>Shows the Pages window.\n")
-    cecho("<yellow>mmpkg config :<white>Shows/Sets Various configuration options.\n")
-    cecho("<yellow>mmpkg update :<white>Check for, download and install updated mmpkg.\n")
+    mmpkg.PagesCon:show()
   end
+elseif (matches[2] == "update") then
+  mmpkg.checkupdate(true)
+else
+  local cmd = matches[2] or ""
+  cecho("<red:white>ERROR:<white:black> Unknown command '"..cmd.."'!\n")
+  cecho("<yellow>mmpkg pages  :<white>Shows the Pages window.\n")
+  cecho("<yellow>mmpkg config :<white>Shows/Sets Various configuration options.\n")
+  cecho("<yellow>mmpkg update :<white>Check for, download and install updated mmpkg.\n")
+end
