6d90de4d5d3a6864eb5ff39303b5bc39c4fd47d1
Chuck Burns <break19@gmail.com>
2020-09-23 08:39:33 -0500

    Rewrite function to count exits/scans for scanhelper

--- mmpkg/src/triggers/mmpkg/GagNone.lua
+++ mmpkg/src/triggers/mmpkg/GagNone.lua
@@ -1,18 +1,20 @@
 local dirs = 0
-local scans = scans or 0
-local scanned = scanned or false
-for _,_ in pairs(mmpkg.fixExits(gmcp.room.info.exits)) do
-  if scanned == true then
-    scanned = false
-    break
+mmpkg.scans = mmpkg.scans or 0
+mmpkg.scanned = true
+if mmpkg.scantimer then
+  killTimer(mmpkg.scantimer)
+end
+mmpkg.scantimer = tempTimer(0, function()
+    mmpkg.scanned = false
+    mmpkg.scans = 0
+  end)
+if mmpkg.scanned == true then
+  for _, _ in pairs(mmpkg.fixExits(gmcp.room.info.exits)) do
+    dirs = dirs + 1
   end
-  dirs = dirs + 1
 end
-scanned = true
-scans = scans + 1
-if scans == dirs then
-  echo("\nScan Complete: Nothing to see here.\n")
-  scans = 0
-  scanned = false
-end
+mmpkg.scans = mmpkg.scans + 1
 deleteLine()
+if mmpkg.scans >= dirs then
+  echo("\nScan Complete: Nothing to see here.\n")
+end
