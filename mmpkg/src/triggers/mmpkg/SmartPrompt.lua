60cab4522d62577d20a214c14327b8357084ed99
harryolney <71859163+harryolney@users.noreply.github.com>
2021-05-02 05:47:56 -0700

    Update SmartPrompt.lua

--- mmpkg/src/triggers/mmpkg/SmartPrompt.lua
+++ mmpkg/src/triggers/mmpkg/SmartPrompt.lua
@@ -1,142 +1,117 @@
 --Do nothing if smart-prompt is disabled, or variables not initialized
 if not mmpkg.conf.smartprompt or not mmpkg.myAffects then
-    return
-  end
-  --Copy2decho the hp/sp/st part of the prompt.
-  local prompt = copy2decho(matches[9])
-  local search = ""
-  local away = ""
-  local tank = ""
-  if matches[2] ~= "" then
-    search = "<yellow>"..matches[2]
-  end
-  if matches[4] ~= "" then
-    away = "<magenta>[Afk]"
-  end
-  if matches[3] ~= "" then
-    -- Show the tank if in combat
-    tank = copy2decho(matches[3])
-  end
-  -- Gag the prompt
-  deleteLine()
-  local mailnews = ""
-  if matches[5] == "[Mail]" then
-    mailnews = mailnews.."<red>[Mail]"
-  end
-  if matches[6] == "[NEWS]" then
-    mailnews = mailnews.."<yellow>[News]"
-  end
-  if matches[7] == "[Library]" then
-    mailnews = mailnews.."<magenta>[Library]"
-  end
-  -- Create our own prompt based on affects, roomflags, and any missing buffs from watchlist
-  local effects = ""
-  local missingprompt = ""
-  local roomflag = roomflag or ""
-  if mmpkg.myAffects.affects then
-    local passdoor, sanc, fireshield, invis, shroud = false, false, false, false, false
-    for _, v in pairs(mmpkg.myAffects.affects) do
-      if v == "pass door" or v == "wind walk" then
-        passdoor = true
-      end
-      if v == "lightning shroud" then
-        shroud = true
-      end
-      if v == "sanctuary" then
-        sanc = true
-      end
-      if v == "improved invisibility" or v == "invisibility" then
-        invis = true
-      end
-      if v == "fireshield" then
-        fireshield = true
-      end
-  
+  return
+end
+--Copy2decho the hp/sp/st part of the prompt.
+local prompt = copy2decho(matches[9])
+local search = ""
+local away = ""
+local tank = ""
+if matches[2] ~= "" then
+  search = "<yellow>"..matches[2]
+end
+if matches[4] ~= "" then
+  away = "<magenta>[Afk]"
+end
+if matches[3] ~= "" then
+  -- Show the tank if in combat
+  tank = copy2decho(matches[3])
+end
+-- Gag the prompt
+deleteLine()
+local mailnews = ""
+if matches[5] == "[Mail]" then
+  mailnews = mailnews.."<red>[Mail]"
+end
+if matches[6] == "[NEWS]" then
+  mailnews = mailnews.."<yellow>[News]"
+end
+if matches[7] == "[Library]" then
+  mailnews = mailnews.."<magenta>[Library]"
+end
+-- Create our own prompt based on affects, roomflags, and any missing buffs from watchlist
+local effects = ""
+local missingprompt = ""
+local roomflag = roomflag or ""
+if mmpkg.myAffects.affects then
+  local passdoor, sanc, fireshield, invis, shroud = false, false, false, false, false
+  for _, v in pairs(mmpkg.myAffects.affects) do
+    if v == "pass door" or v == "wind walk" then
+      passdoor = true
     end
-    -- keep effects in a standard order.
-    if passdoor then
-      effects = effects .. "<cyan>*"
+    if v == "lightning shroud" then
+      shroud = true
     end
-    if sanc then
-      effects = effects .. "<white>*"
+    if v == "sanctuary" then
+      sanc = true
     end
-    if fireshield then
-      effects = effects .. "<red>*"
+    if v == "improved invisibility" or v == "invisibility" then
+      invis = true
     end
-  
-    if shroud then
-      effects = effects .. "<blue>*"
+    if v == "fireshield" then
+      fireshield = true
     end
-    if invis then
-      effects = effects .. "<green>"
+
+  end
+  -- keep effects in a standard order.
+  if passdoor then
+    effects = effects .. "<cyan>*"
+  end
+  if sanc then
+    effects = effects .. "<white>*"
+  end
+  if fireshield then
+    effects = effects .. "<red>*"
+  end
+
+  if shroud then
+    effects = effects .. "<blue>*"
+  end
+  if invis then
+    effects = effects .. "<green>*"
+  end
+  wanted = mmpkg.conf.wantedbuffs
+  have = mmpkg.myAffects.affects
+  if mmpkg.conf.buffwatcher then
+    local missing, missingshort = mmpkg.buffwatcher:getMissingBuffs(wanted, have)
+    if not table.is_empty(missingshort) then
+      local spells = table.concat(missingshort, "' '")
+      missingprompt = string.format(" <red>M: <white>'%s'", spells)
     end
-    wanted = mmpkg.conf.wantedbuffs
-    have = mmpkg.myAffects.affects
-    if mmpkg.conf.buffwatcher then
-      local missing, missingshort = mmpkg.buffwatcher:getMissingBuffs(wanted, have)
-      if not table.is_empty(missingshort) then
-        local spells = table.concat(missingshort, "' '")
-        missingprompt = string.format(" <red>M: <white>'%s'", spells)
-      end
-    end
   end
-  local roomflags = getRoomUserDataKeys(gmcp.room.info.num)
-  if matches[8] == "[SAFE]" then
-    roomflag = roomflag.."<white>[Safe]"
-  end
-  if table.contains(roomflags, "shop") then
-    roomflag = roomflag.."<yellow>[Shop]"
-  end
-  if table.contains(roomflags, "trainer") then
-    roomflag = roomflag.."<blue>[Trainer]"
-  end
-  if table.contains(roomflags, "high-regen") then
-    roomflag = roomflag.."<cyan>[HiRgn]"
-  end
-  if table.contains(roomflags, "low-regen") then
-    roomflag = roomflag.."<firebrick>[LoRgn]"
-  end
-  if table.contains(roomflags, "no-regen") then
-    roomflag = roomflag.."<red>[NoRgn]"
-  end
-  if table.contains(roomflags, "half-pk-damage") then
-    roomflag = roomflag.."<grey>[HalfDmgPk]"
-  end
-  if table.contains(roomflags, "double-pk-damage") then
-    roomflag = roomflag.."<grey>[DblDmgPk]"
-  end
-  if table.contains(roomflags, "no-form") then
-    roomflag = roomflag.."<grey>[NoForm]"
-  end
-  if table.contains(roomflags, "no-trap") then
-    roomflag = roomflag.."<grey>[NoTrap]"
-  end
-  if table.contains(roomflags, "anti-magic") then
-    roomflag = roomflag.."<grey>[NoMag]"
-  end
-  if table.contains(roomflags, "silence") then
-    roomflag = roomflag.."<grey>[Silent]"
-  end
-  if table.contains(roomflags, "icy-conditions") then
-    roomflag = roomflag.."<cyan>[Icy]"
-  end
-  if table.contains(roomflags, "no-npcs") then
-    roomflag = roomflag.."<grey>[NoNPC]"
-  end
-  if table.contains(roomflags, "no-recall") then
-    roomflag = roomflag.."<grey>[NoRecall]"
-  end
-  if table.contains(roomflags, "player-kill-neutral") then
-    roomflag = roomflag.."<red>[NPK]"
-  end
-  if table.contains(roomflags, "player-kill-lawful") then
-    roomflag = roomflag.."<red>[LPK]"
-  end
-  if table.contains(roomflags, "player-kill-chaotic" ) or table.contains(roomflags, "CPK") then
-    roomflag = roomflag.."<red>[CPK]"
-  end
-  cecho("\n" .. effects .. away ..search .. mailnews .. roomflag)
-  decho(tank..prompt)
-  cecho(missingprompt)
-  echo("\n")
-  
+end
+local roomflags = getRoomUserDataKeys(gmcp.room.info.num)
+if matches[8] == "[SAFE]" then
+  roomflag = roomflag.."<white>[Safe]"
+end
+if table.contains(roomflags, "shop") then
+  roomflag = roomflag.."<yellow>[Shop]"
+end
+if table.contains(roomflags, "trainer") then
+  roomflag = roomflag.."<blue>[Trainer]"
+end
+if table.contains(roomflags, "high-regen") then
+  roomflag = roomflag.."<cyan>[HiRgn]"
+end
+if table.contains(roomflags, "low-regen") then
+  roomflag = roomflag.."<firebrick>[LoRgn]"
+end
+if table.contains(roomflags, "no-regen") then
+  roomflag = roomflag.."<red>[NoRgn]"
+end
+if table.contains(roomflags, "no-trap") then
+  roomflag = roomflag.."<red>[NoTrap]"
+end
+if table.contains(roomflags, "player-kill-neutral") table.contains(roomflags, "NPK") then
+  roomflag = roomflag.."<red>[NPK]"
+end
+if table.contains(roomflags, "player-kill-lawful") table.contains(roomflags, "LPK") then
+  roomflag = roomflag.."<red>[LPK]"
+end
+if table.contains(roomflags, "player-kill-chaotic") or table.contains(roomflags, "CPK") then
+  roomflag = roomflag.."<red>[CPK]"
+end
+cecho("\n" .. effects .. away ..search .. mailnews .. roomflag)
+decho(tank..prompt)
+cecho(missingprompt)
+echo("\n")
